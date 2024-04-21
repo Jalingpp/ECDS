95f551601d9268445ec00e5e581c423f749f6ff7
Jalingpp <jpjia22@stu.ecnu.edu.cn>
2024-04-21 09:12:41 +0000

    test signature

---
+++ encoding/ec.go
@@ -0,0 +1,93 @@
+package encoding
+
+import (
+	"fmt"
+
+	"github.com/templexxx/reedsolomon"
+)
+
+func TestEC() {
+	//创建编码器
+	encoder, err := reedsolomon.New(5, 3)
+	if err != nil {
+		panic(err)
+	}
+	//创建数据分片
+	data := make([][]byte, 8)
+	//初始化数据分片
+	for i := range data {
+		data[i] = make([]byte, 7)
+	}
+	data[0] = []byte("abcdefg")
+	data[1] = []byte("hijklmn")
+	data[2] = []byte("opqrstu")
+	data[3] = []byte("vwxyz01")
+	data[4] = []byte("2345678")
+	//RS纠删码编码
+	err = encoder.Encode(data)
+	if err != nil {
+		panic(err)
+	}
+	// Print the data
+	for i := range data {
+		fmt.Println(i, string(data[i]))
+	}
+	// //验证数据分片可恢复性
+	// ok, err1 := encoder.Verify(data)
+	// if err1 != nil {
+	// 	panic(err1)
+	// }
+	// fmt.Println("Verify:", ok)
+	// //删除数据分片
+	// data[1] = nil
+	// // Print the data
+	// for i := range data {
+	// 	fmt.Println(i, string(data[i]))
+	// }
+	// //重建数据分片
+	// err = encoder.Reconstruct(data)
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// // Print the data
+	// for i := range data {
+	// 	fmt.Println(i, string(data[i]))
+	// }
+	//生成一个对比数据分片
+	compdata := make([][]byte, 8)
+	for i := range compdata {
+		compdata[i] = make([]byte, 7)
+		copy(compdata[i], data[i])
+	}
+	//Print the compdate
+	for i := range compdata {
+		fmt.Println(i, string(compdata[i]))
+	}
+	//更新数据分片
+	newdata := []byte("1234567")
+	updaterow := 1
+	parityData := make([][]byte, 3)
+	parityData[0] = make([]byte, 7)
+	parityData[1] = data[6]
+	parityData[2] = make([]byte, 7)
+	err = encoder.Update(data[updaterow], newdata, updaterow, parityData)
+	if err != nil {
+		panic(err)
+	}
+	// Print the data
+	for i := range data {
+		fmt.Println(i, string(data[i]))
+	}
+
+	//修改compdata，重编码
+	compdata[updaterow] = newdata
+	err = encoder.Encode(compdata)
+	if err != nil {
+		panic(err)
+	}
+	//Print the compdate
+	for i := range compdata {
+		fmt.Println(i, string(compdata[i]))
+	}
+
+}
--- signature/bls.go
+++ signature/bls.go
@@ -1,9 +1,61 @@
 package signature
 
-import "github.com/Nik-U/pbc"
+import (
+	"crypto/sha256"
+	"github.com/Nik-U/pbc"
+	"fmt"
+)
 
-func testSignature() {
+type PDP struct{
+	Pairing *pbc.Pairing
+	G []byte
+	PrivKey []byte
+	PubKey []byte
+}
+
+func (pdp *PDP) Setup(){
+	params := pbc.GenerateA(160,512).String()
+	pairing, _ := pbc.NewPairingFromString(params)
+	pdp.Pairing = pairing
+	pdp.G = pdp.Pairing.NewG2().Rand().Bytes()
+	pdp.PrivKey = pdp.Pairing.NewZr().Rand().Bytes()
+	pdp.PubKey = pdp.Pairing.NewG2().PowZn(pdp.G,pdp.PrivKey).Bytes()
+}
+
+func (pdp *PDP) GetSig(message string) []byte{
+	h := pdp.Pairing.NewG1().SetFromStringHash(message,sha256.New())
+	privkey := pdp.Pairing.NewZr().SetBytes(pdp.PrivKey)
+	signature := pdp.Pairing.NewG2().PowZn(h,privkey)
+	return signature.Bytes()
+}
+
+func VerifySig(pairing *pbc.Pairing,gb []byte,pubkey []byte, message string, signature []byte) bool{
+	sig := pairing.NewG1().SetBytes(signature)
+	h := pairing.NewG1().SetFromStringHash(message,sha256.New())
+	pk := pairing.NewG2().SetBytes(pubkey)
+	g := pairing.NewG2().SetBytes(gb)
+	temp1 := pairing.NewGT().Pair(h,pk)
+	temp2 := pairing.NewGT().Pair(sig,g)
+	if !temp1.Equals(temp2){
+		fmt.Println("*BUG* Signature check failed *BUG*")
+		return false
+	}else{
+		fmt.Println("Signature verified correctly")
+		return true
+	}
+}
+
+func TestSignature() {
 	//setup
-	pairing,err := pbc.NewPairingFromString("type a\nq 87807107996633125224377819847540498158068831994142082\n")
-
+	params := pbc.GenerateA(160,512)
+	pairing := params.NewPairing()
+	g := pairing.NewG1()
+	h := pairing.NewG2()
+	x := pairing.NewGT()
+	g.Rand()
+	h.Rand()
+	fmt.Printf("g=%s\n",g)
+	fmt.Printf("h=%s\n",h)
+	x.Pair(g,h)
+	fmt.Printf("e(g,h)=%s\n",x)
 }
